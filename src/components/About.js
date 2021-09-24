00855774be1c4f90d0275070f3e3686f810f91f8
javier vilchis <javivilchis@gmail.com>
2021-09-23 21:46:17 -0500

    adding files to the main folder

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,10 @@ import React from "react";
 function About() {
      return (
           <section id="about-me">
-               <div class="side">
+               <div className="side">
                     <h2>About Me</h2>
                </div>
-               <div class="content">
+               <div className="content">
                     <p>Javier Vilchis is a Web Developer and a Multimedia Designer with focus on User Experience on a
                          Marketing Level. He has the ability to strategize your idea into a full marketing campaing to
                          get your business running at it's best!. He is in the process of acquiring his Certified ANA
---
+++ src/images/index1.js
@@ -0,0 +1,10 @@
+export default {
+     header: require('./header-image.png').default,
+     home: {
+          logo: require('').default
+     },
+     portfolio: {
+          ambitcares: require('./ambitcares-website.png').default,
+
+     }
+}
