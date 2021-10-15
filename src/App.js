a0fe0599879fe78a803342f2497d834781e2de63
javier vilchis <javivilchis@gmail.com>
2021-10-15 08:41:34 -0500

    portfolio site

--- src/App.js
+++ src/App.js
@@ -9,14 +9,32 @@ import NavBar from "./NavBar";
 import Hero from "./Hero";
 import Footer from "./Footer";
 import { Switch, Route } from "react-router-dom";
-
 import ReactGA from 'react-ga';
+/* importing firebase */
+import firebase from 'firebase/compat/app';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCcfWq3jlxxrSoadsHSCwb3zKZfMNhgkAs",
+  authDomain: "jv-website-42ff6.firebaseapp.com",
+  databaseURL: "https://jv-website-42ff6.firebaseio.com",
+  projectId: "jv-website-42ff6",
+  storageBucket: "jv-website-42ff6.appspot.com",
+  messagingSenderId: "915417051276",
+  appId: "1:915417051276:web:ef203d8b7d73a0430a4ba9",
+  measurementId: "G-DFB3DJ2HT5"
+});
+
+
+/* iimporting react Google Analytics */
+
 
 const trackingId = "G-ECYMCZPMG0"; 
 ReactGA.initialize(trackingId);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
+  
   return (
     <div className="App">
       <NavBar />
