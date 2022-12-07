40f5f479e8716c7fa284eb4bc480ba95f8057a67
Javier Vilchis <javivilchis@gmail.com>
2022-12-07 09:08:54 -0600

    Add files via upload

---
+++ precache-manifest.50ed1e835b551ba561b3196d9291ac11.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8100d1c00652082b58469e7429176a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66c12a7c100f2d47b781",
+    "url": "./static/css/2.e3f1d676.chunk.css"
+  },
+  {
+    "revision": "4a31acfa0b4308526ea3",
+    "url": "./static/css/main.724382fd.chunk.css"
+  },
+  {
+    "revision": "66c12a7c100f2d47b781",
+    "url": "./static/js/2.43c6a9d8.chunk.js"
+  },
+  {
+    "revision": "1f2184db67a5ed089b4ad09d3682c2c0",
+    "url": "./static/js/2.43c6a9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a31acfa0b4308526ea3",
+    "url": "./static/js/main.a4f29dda.chunk.js"
+  },
+  {
+    "revision": "9f43b963a77d7f134499",
+    "url": "./static/js/runtime-main.8d46570a.js"
+  },
+  {
+    "revision": "026f5b388db4125ed4e99a50a50fe77e",
+    "url": "./static/media/ambitcares-website.026f5b38.png"
+  },
+  {
+    "revision": "4207e6d7af4a3ec0b323ae52c0547f19",
+    "url": "./static/media/ambitsuccess-website.4207e6d7.png"
+  },
+  {
+    "revision": "13b4ec6d46c6c2d850efcfe4cca66f2f",
+    "url": "./static/media/header-image.13b4ec6d.png"
+  },
+  {
+    "revision": "cdc951bb2cd6d9691e6ec120b8ffe765",
+    "url": "./static/media/javier_vilchis_duo_purple.cdc951bb.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "79e3520d3fc88fa07659e07245f12387",
+    "url": "./static/media/note-taker-heroku.79e3520d.png"
+  },
+  {
+    "revision": "71b20ccdce202c51ad7b69d29d71ea0d",
+    "url": "./static/media/poppies.71b20ccd.jpeg"
+  },
+  {
+    "revision": "98bc7f362052374ca8d50864db46408b",
+    "url": "./static/media/puzzle.98bc7f36.png"
+  }
+]);
