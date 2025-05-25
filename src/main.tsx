7dee195ec55689b8e1c35063de272eb37a5769a4
Javier Vilchis <javivilchis@gmail.com>
2025-05-25 09:13:30 -0500

    adding all new files

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// ✅ Redirect from GitHub Pages 404.html
+const redirect = window.location.search
+  ? window.location.search.slice(1) // removes the "?" and keeps the path
+  : undefined;
+
+if (redirect && window.history.replaceState) {
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
